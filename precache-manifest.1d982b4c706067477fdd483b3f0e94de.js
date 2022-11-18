b78d7e7715476448776c5c79e5e5606b91fbf334
jcaluiz <lacjunior129.la@gmail.com>
2022-11-18 14:32:26 -0300

    Updates

---
+++ precache-manifest.1d982b4c706067477fdd483b3f0e94de.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b62c25680806bda9135",
+    "url": "//jcaluiz.github.io/jcaluiz.github.io/static/css/main.7366d5dd.chunk.css"
+  },
+  {
+    "revision": "1b62c25680806bda9135",
+    "url": "//jcaluiz.github.io/jcaluiz.github.io/static/js/main.1b62c256.chunk.js"
+  },
+  {
+    "revision": "e4d61840aaa51948051c",
+    "url": "//jcaluiz.github.io/jcaluiz.github.io/static/js/1.e4d61840.chunk.js"
+  },
+  {
+    "revision": "4774ebde2238f6beecdd",
+    "url": "//jcaluiz.github.io/jcaluiz.github.io/static/js/runtime~main.4774ebde.js"
+  },
+  {
+    "revision": "bbb56b2642d221abcce52672a7a39ef6",
+    "url": "//jcaluiz.github.io/jcaluiz.github.io/static/media/minha-foto.bbb56b26.png"
+  },
+  {
+    "revision": "63de5479e8eb4676570c49e2579cab01",
+    "url": "//jcaluiz.github.io/jcaluiz.github.io/static/media/github.63de5479.png"
+  },
+  {
+    "revision": "19455d676899e370bd5cc0bab6b41c6f",
+    "url": "//jcaluiz.github.io/jcaluiz.github.io/static/media/whatsapp.19455d67.png"
+  },
+  {
+    "revision": "57ba880fa6ae032f1a2bd52de181fded",
+    "url": "//jcaluiz.github.io/jcaluiz.github.io/static/media/trivia-project-1.57ba880f.png"
+  },
+  {
+    "revision": "cf8ab780edba7edd46087aff49860f93",
+    "url": "//jcaluiz.github.io/jcaluiz.github.io/static/media/shop-cart-project.cf8ab780.png"
+  },
+  {
+    "revision": "9c18c84982a6598682194b0acea8099d",
+    "url": "//jcaluiz.github.io/jcaluiz.github.io/index.html"
+  }
+];
