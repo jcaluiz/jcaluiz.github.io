ceb3c7f3d39e5cd9a0ca00b6954a8297c5dc5f21
jcaluiz <lacjunior129.la@gmail.com>
2022-11-21 18:11:45 -0300

    refatorando css responsivo

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 export default function Contact() {
     return (
         <>
             <Header />
             <h1>A página de contato está em construção</h1>
+            <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
             <Footer />
         </>
     )
