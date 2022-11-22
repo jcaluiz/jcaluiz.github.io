bff92113e21e01d8a6ed0ae8e64cc5ea7e21e664
jcaluiz <lacjunior129.la@gmail.com>
2022-11-22 08:28:46 -0300

    ajeitando a página de projetos

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -9,20 +9,28 @@ export default function Projects() {
     return (
         <>
             <Header />
-            <h1>Página de projetos</h1>
-            <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src={starWars} />
-                <Card.Body>
-                    <Card.Title>Projeto Star Wars Search</Card.Title>
-                    <Card.Text>
-                        Neste projeto feito em React.js foi desenvolvido 
-                        uma lista com filtros de planetas do universo de Star Wars.
-                    </Card.Text>
-                    <a href="https://jcaluiz.github.io/Project-StartWars-Planet-Search/" target='_blank' rel='noreferrer'>
-                        <Button variant="primary">Clique para ir a página</Button>
-                    </a>
-                </Card.Body>
-            </Card>
+            <main className="main-project">
+                <h1>Página de projetos</h1>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src={starWars} />
+                    <Card.Body>
+                        <Card.Title>Projeto Star Wars Search</Card.Title>
+                        <Card.Text>
+                            Neste projeto feito em React.js foi desenvolvido
+                            uma lista com filtros de planetas do universo de Star Wars.
+                        </Card.Text>
+                        <div className="button-project-container">
+                            <a href="https://jcaluiz.github.io/Project-StartWars-Planet-Search/" target='_blank' rel='noreferrer'>
+                                <Button variant="primary">Ir à página</Button>
+                            </a>
+
+                            <a href="https://github.com/jcaluiz/Project-StartWars-Planet-Search" target='_blank' rel='noreferrer'>
+                                <Button variant="primary">Ir para o Repositório GitHub</Button>
+                            </a>
+                        </div>
+                    </Card.Body>
+                </Card>
+            </main>
             <Footer />
         </>
     )
