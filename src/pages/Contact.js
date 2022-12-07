9c9ff2a7f1f1c910aa78e3da5bf72277e9aa3262
jcaluiz <lacjunior129.la@gmail.com>
2022-12-07 12:07:10 -0300

    criando as funcionalidades de contato da página de contato

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,34 +1,86 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
-// import Button from 'react-bootstrap/Button';
-// import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import emailjs from '@emailjs/browser';
 
 export default function Contact() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+        if (name === '' || email === '' || message === '') {
+            alert('É obrigatório preencher todos os campos');
+            return;
+        }
+        const templateParams = {
+            from_name: name,
+            message,
+            email,
+        }
+        emailjs.send(
+            'service_1q05vh9',
+            'template_osutrmq',
+            templateParams,
+            'ZpAtKJTwCgMHOQaAD'
+        ).then((response) => {
+            console.log('EMAIL ENVIADO', response.status, response.text);
+            setName('');
+            setEmail('');
+            setMessage('');
+        }, (err) => {
+            console.log('ERRO: ', err);
+        });
+    }
     return (
         <>
             <Header />
-            <h1>A página de contato está em construção</h1>
-            {/* <Form>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                    </Form.Text>
-                </Form.Group>
-
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
-            </Form> */}
+            <main className="main-contact">
+                <h1 id="title-contact-page">Deixe aqui a sua mensagem</h1>
+                <Form className="forms-container" onSubmit={sendEmail}>
+                    <Form.Group
+                        className="mb-3"
+                        controlId="Digite o seu nome"
+                    >
+                        <Form.Label>Nome</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="Digite seu nome"
+                            onChange={(e) => setName(e.target.value)}
+                            value={name}
+                        />
+                    </Form.Group>
+                    <Form.Group
+                        className="mb-3"
+                        controlId="exampleForm.ControlInput1">
+                        <Form.Label>Endereço de Email</Form.Label>
+                        <Form.Control
+                            type="email"
+                            placeholder="name@example.com"
+                            onChange={(e) => setEmail(e.target.value)}
+                            value={email}
+                        />
+                    </Form.Group>
+                    <Form.Group
+                        className="mb-3"
+                        controlId="exampleForm.ControlTextarea1">
+                        <Form.Label>Example textarea</Form.Label>
+                        <Form.Control
+                            as="textarea"
+                            onChange={(e) => setMessage(e.target.value)}
+                            value={message}
+                            rows={10}
+                        />
+                    </Form.Group>
+                    <Button
+                        variant="outline-primary"
+                        type="submit"
+                    >Enviar</Button>{' '}
+                </Form>
+            </main>
             <Footer />
         </>
     )
